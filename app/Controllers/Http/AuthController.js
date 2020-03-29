2686ea84ca95afad823a800a14ccc93d503df4cb
Alex TEA <alextea2@gmail.com>
2020-03-30 01:28:48 +0200

    init w/ user authentication

---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Persona = use('Persona');
+
+class AuthController {
+
+  async register ({ request, response }) {
+    const payload = request.only(['email', 'password', 'password_confirmation', 'firstname', 'lastname']);
+    const user = await Persona.register(payload);
+    return response.ok({
+      user,
+      status: 200,
+      message: 'Client account created successfully',
+    })
+  }
+
+  async login ({ request, auth, response }) {
+    const payload = request.only(['uid', 'password']);
+    const { uid, password } = payload;
+    await Persona.verify(payload);
+    const { token } = await auth.attempt(uid, password);
+    return response.ok({ token });
+  }
+
+  async forgotPassword ({ request, response }) {
+    await Persona.forgotPassword(request.input('uid'));
+  }
+
+  async updatePasswordByToken ({ request }) {
+    const token = request.input('token');
+    const payload = request.only(['password', 'password_confirmation']);
+    await Persona.updatePasswordByToken(token, payload);
+  }
+
+}
+
+module.exports = AuthController;
---
+++ docker-start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "\n\n\nNpm install:"
+yarn install
+
+echo "\n\n\nRun migration:"
+adonis migration:run --force
+
+echo "\n\n\nStart node server:"
+adonis serve --dev --polling

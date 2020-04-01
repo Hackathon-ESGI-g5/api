d4b050d25f3d8c2658d22aace111d25779a1a2be
Soufian AIT TIRITE <contact@soufian-a.net>
2020-04-01 17:23:58 +0200

    Post controller

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,28 +1,123 @@
 'use strict'
 
 class PostController {
-    async getByid({ request, auth, response }) {
-
+    async getById({ request, params, auth, response }) {
+        const postId = params.postId;
+        const post = await Post.find(postId);
+        if(post != null){
+            return response.status(200).json({
+                status: "Success",
+                post
+            });
+        } else {
+            return response.status(404).json({
+                status: "Error",
+                message: "Post not found"
+            });
+        }
     }
 
     async getByStore({ request, auth, response }){
-        
+        const storeId = params.storeId;
+        const post = await Post.query().where("store_id",storeId).first();
+        if(post != null){
+            return response.status(200).json({
+                status: "Success",
+                post
+            });
+        } else {
+            return response.status(404).json({
+                status: "Error",
+                message: "Post not found"
+            });
+        }
     }
 
-    async getAll({ request, auth, response }){
-        
+    async getAll({ response }){
+        const posts = await Post.all();
+        return response.status(200).json({
+            status: "Success",
+            rows: posts.rows.length,
+            posts
+        });
     }
 
-    async create({ request, auth, response }){
-
+    async create({ request, auth, response, params }){
+        const shopId = params.shopId;
+        const { title, body, status } = request.post();
+        const post = new Post();
+        post.title = title;
+        post.body = body;
+        post.status = status;
+        try{
+            await post.save();
+            return response.status(201).json({
+                status: "Success",
+                post
+            });
+        } catch(e) {
+            return response.status(400).json({
+                status: "Error",
+                message: "An error occured on create Post",
+                stack_trace: e
+            });
+        }
     }
 
     async update({ request, auth, response }){
-
+        const postId = params.postId;
+        const { title, body, status } = request.post();
+        const post = await Post.find(postId);
+        if(post != null){
+            post.title = title;
+            post.body = body;
+            post.status = status;
+            try{
+                await post.save();
+                return response.status(200).json({
+                    status: "Update succeed",
+                    post
+                });
+            } catch(e) {
+                return response.status(400).json({
+                    status: "Error",
+                    message: "An error occured on update Post",
+                    stack_trace: e
+                });
+            }
+        } else {
+            return response.status(404).json({
+                status: "Error",
+                message: "Role not found",
+                stack_trace: e
+            });
+        }
     }
 
     async delete({ request, auth, response }){
-        
+        const postId = params.postId;
+        const post = await Post.find(postId);
+        if(post != null){
+            try{
+                await post.delete();
+                return response.status(200).json({
+                    status: "Deletion succeed",
+                    post
+                });
+            } catch(e) {
+                return response.status(400).json({
+                    status: "Error",
+                    message: "An error occured on delete Post",
+                    stack_trace: e
+                });
+            }
+        } else {
+            return response.status(404).json({
+                status: "Error",
+                message: "Post not found",
+                stack_trace: e
+            });
+        }
     }
 }
 

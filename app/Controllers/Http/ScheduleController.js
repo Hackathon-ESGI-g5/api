5c991df43c261fa97b961c1de8ec658eae00af65
Soufian AIT TIRITE <contact@soufian-a.net>
2020-04-02 15:11:42 +0200

    fix for function name on schedulCOntroller

--- app/Controllers/Http/ScheduleController.js
+++ app/Controllers/Http/ScheduleController.js
@@ -6,7 +6,7 @@ const SlotController = use('App/Controllers/Http/SlotController');
 const moment = use('moment')
 
 class ScheduleController {
-    async getByid({ request, auth, response }) {
+    async getById({ request, auth, response }) {
         const scheculeId = params.scheduleId;
         const schedule = await Schedule.find(scheculeId);
         if(schedule != null){

20ebfeb12eabb1f66bfb5ee2382adf93ce0f9651
Soufian AIT TIRITE <contact@soufian-a.net>
2020-04-02 09:48:59 +0200

    update UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -28,16 +28,46 @@ class UserController {
         }
     }
 
-    async create({ request, auth, response }){
-
-    }
-
     async update({ request, auth, response }){
-        
+        const payload = request.only(['email', 'password', 'password_confirmation', 'firstname', 'lastname', 'profil_picture_url', 'role_id']);
+        try {
+            const user = await Persona.register(payload);
+            return response.status(200).json({
+                user,
+                status: 200,
+                message: 'Client account created successfully',
+            })
+        } catch(e) {
+            return response.status(400).json({
+                status: 400,
+                message: 'Error on registration',
+            })
+        }
     }
 
     async delete({ request, auth, response }){
-        
+        const userId = params.userId;
+        const user = await User.find(userId);
+        if(user != null){
+            try{
+                await user.delete();
+                return response.status(200).json({
+                    status: "Deletion succeed"
+                });
+            } catch(e) {
+                return response.status(400).json({
+                    status: "Error",
+                    message: "An error occured on delete User",
+                    stack_trace: e
+                });
+            }
+        } else {
+            return response.status(404).json({
+                status: "Error",
+                message: "User not found",
+                stack_trace: e
+            });
+        }
     }
 }
 

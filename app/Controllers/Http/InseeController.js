277684537b5118d2971cf3c3c20893ba024b2c5c
Soufian AIT TIRITE <contact@soufian-a.net>
2020-04-04 19:31:42 +0200

    fix Insee controller

--- app/Controllers/Http/InseeController.js
+++ app/Controllers/Http/InseeController.js
@@ -19,48 +19,45 @@ class InseeController {
             return response.status(rp.header.statut).json(rp);
         }).catch(async err => {
             console.log(err);
-            try{
-                await this.generateToken({params,response});
+            await this.generateToken().then((value) => {
                 return this.getBySiret({params,response});
-            } catch(e){
+            }).catch((e) => {
                 return response.status(400).json({
                     status: "Error",
                     message: "Insee API currently not available",
                     stack_trace: e.message
                 });
-            }
+            });
         });
     }
 
-    async generateToken({params,response}){
+    async generateToken(){
         //Config
         console.log('GENERATE INSEE TOKEN');
         //const bas64 = Base64(Env.get('INSEE_CONSUMER_KEY')+":"+Env.get('INSEE_CONSUMER_SECRET'));
-        const bas64 = "plop"//Buffer.from(Env.get('INSEE_CONSUMER_KEY')+":"+Env.get('INSEE_CONSUMER_SECRET')).toString('base64');
-        await fetch('https://api.insee.fr/token?grant_type=client_credentials&validity_period=2400000',{
-            method: 'POST',
-            headers: {
-                Authorization: `Basic ${bas64}`
-            }
-        }).then(async res => {
-            var rp = await res.json();
-            console.log("INSEE TOKEN GENERATED !",rp);
-            if(rp.access_token != null){
-                await Config.set('insee.key', rp.access_token);
-                console.log("INSEE TOKEN STORED !",Config.get('insee.key'));
-                return;
-            } else {
-                console.log("INSEE THROW");
-                throw new Error('Token not given, error on format or credentials to INSEE API');
-            }
-        }).catch(async err => {
-            console.log("ERROR ON INSEE TOKEN GENERATION : ",err);
-            return response.status(400).json({
-                status: "Error",
-                message: "Insee API currently not available",
-                stack_trace: err.message
+        const bas64 = Buffer.from(Env.get('INSEE_CONSUMER_KEY')+":"+Env.get('INSEE_CONSUMER_SECRET')).toString('base64');
+        return new Promise(async (resolve, reject) => {
+            await fetch('https://api.insee.fr/token?grant_type=client_credentials&validity_period=2400000',{
+                method: 'POST',
+                headers: {
+                    Authorization: `Basic ${bas64}`
+                }
+            }).then(async res => {
+                var rp = await res.json();
+                console.log("INSEE TOKEN GENERATED !",rp);
+                if(rp.access_token != null){
+                    await Config.set('insee.key', rp.access_token);
+                    console.log("INSEE TOKEN STORED !",Config.get('insee.key'));
+                    resolve();
+                } else {
+                    console.log("INSEE THROW");
+                    reject('Token not given, error on format or credentials to INSEE API');
+                }
+            }).catch(async err => {
+                console.log("ERROR ON INSEE TOKEN GENERATION : ",err);
+                reject(err.message);
             });
-        });        
+        })
     }
 }
 
